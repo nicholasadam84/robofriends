0bd195e6958ee8e502abc569bf1c9ecbe8ae62b7
nicholasadam84 <nicholasadam84@gmail.com>
2019-12-19 11:57:10 -0800

    added gradient background and h1 styling from google fonts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
+import './App.css'
 
 
 //Props - things that come out of State.
@@ -47,7 +48,7 @@ class App extends Component {
         })
         return (
             <div className='tc'>
-                <h1>Robofriends</h1>
+                <h1 className='f1'>Robofriends!</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filteredRobots} />
             </div>
