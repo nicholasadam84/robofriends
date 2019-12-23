a972168902082d0c20f907b85809004eb05b8ff2
nicholasadam84 <nicholasadam84@gmail.com>
2019-12-23 10:58:17 -0800

    Updates

---
+++ precache-manifest.d251334c5a1221d20b74316e317f8a6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb2e111799f36ded9142ecc92c799cb",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7c58d725acf7de7c97d7",
+    "url": "/robofriends/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "ff1e085be8257a0a8d9c",
+    "url": "/robofriends/static/css/main.0fac8666.chunk.css"
+  },
+  {
+    "revision": "7c58d725acf7de7c97d7",
+    "url": "/robofriends/static/js/2.f0d44eb8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.f0d44eb8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff1e085be8257a0a8d9c",
+    "url": "/robofriends/static/js/main.7c9091c5.chunk.js"
+  },
+  {
+    "revision": "04454491b22da9ad3e7d",
+    "url": "/robofriends/static/js/runtime-main.98228a54.js"
+  }
+]);
